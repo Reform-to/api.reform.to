96df8cbdc1fb3b09d75462f49245198837b32984
Christopher Adams <chris@raysend.com>
2014-05-05 18:50:13 +0800

    Allow file extension to determine accept media type

--- app/tests/integration/net/http/RouterTest.php
+++ app/tests/integration/net/http/RouterTest.php
@@ -52,6 +52,24 @@ class RouterTest extends \lithium\test\Unit {
 
     }
 
+    /**
+     * We should be able to participate in content negotiation by using the
+     * Accept header. However, some clients don't have an easy time with this.
+     * To be practical, we can also use a file extension as a way to
+     * communicate the desired media type.
+     */
+
+    public function testReformersWithMediaType() {
+
+        $expected = array('controller' => 'Reformers', 'action' => 'index', 'args' => array(0 => ''), 'type' => 'json');
+
+        $request = new Request(array('url' => '/reformers/index.json'));
+
+        $result = Router::process($request);
+        $this->assertEqual($expected, $result->params);
+
+    }
+
 }
 
 ?>
