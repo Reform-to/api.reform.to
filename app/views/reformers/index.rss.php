58c51099f0f037a391396fdbb43f9494c01c0ed1
Christopher Adams <chris@raysend.com>
2014-07-08 17:22:52 +0800

    Fix typo in RSS description

--- app/views/reformers/index.rss.php
+++ app/views/reformers/index.rss.php
@@ -2,7 +2,7 @@
     <?php $pubDate = new DateTime($reformer->verification->date_modified); ?>
     <item>
         <title><?=$reformer->full_name(); ?></title>
-         <description><?=$reformer->full_name(); ?> has pledge support for fundamental reform.</description>
+         <description><?=$reformer->full_name(); ?> has pledged support for fundamental reform.</description>
          <link><?=$reformer->url(); ?></link>
          <pubDate><?=$pubDate->format(DateTime::RSS); ?></pubDate>
          <guid isPermaLink="true"><?=$reformer->url(); ?></guid>
