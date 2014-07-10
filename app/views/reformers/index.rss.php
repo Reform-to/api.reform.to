49ae5505af16756fe8f272bb1239cce1f65097f5
Christopher Adams <chris@raysend.com>
2014-07-10 14:16:29 +0800

    Use RSS item description as title

--- app/views/reformers/index.rss.php
+++ app/views/reformers/index.rss.php
@@ -2,7 +2,7 @@
     $pubDate = new DateTime($reformer->verification->date_modified);
     $from_state = $reformer->state ? "from $reformer->state" : '';
     $pledge_count = $reformer->pledges->count();
-    $description = implode(array_filter(array(
+    $title = implode(array_filter(array(
         $reformer->title(),
         $reformer->full_name(),
         $from_state,
@@ -13,8 +13,8 @@
 ?>
 
     <item>
-        <title><?=$reformer->full_name(); ?></title>
-         <description><?=$description; ?></description>
+         <title><?=$title; ?></title>
+         <description></description>
          <link><?=$reformer->url(); ?></link>
          <pubDate><?=$pubDate->format(DateTime::RSS); ?></pubDate>
          <guid isPermaLink="true"><?=$reformer->url(); ?></guid>
